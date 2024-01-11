29e83b9b8de457d2fd1d0f3dd5728b71102c7730
ArtMedici <zcgherty@ya.ru>
2024-01-12 01:24:28 +0500

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,6 @@
+// https://forkify-api.herokuapp.com/v2
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const KEY = '8f53ed72-45b8-4d65-83a0-6a3ca26439d2';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const MODAL_CLOSE_SEC = 2.5;
